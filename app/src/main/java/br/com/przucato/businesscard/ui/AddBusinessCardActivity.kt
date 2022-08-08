a4e1b4d651841186045f80f577697f82d700387d
przucato <pri.zucato@hotmail.com>
2022-08-07 21:13:47 -0300

    Create AddBusinessCardActivity and layout

--- app/src/main/java/br/com/przucato/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/br/com/przucato/businesscard/ui/AddBusinessCardActivity.kt
@@ -2,14 +2,14 @@ package br.com.przucato.businesscard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import br.com.przucato.businesscard.databinding.ActivityMainBinding
+import br.com.przucato.businesscard.databinding.ActivityAddBusinessCardBinding
 
-class MainActivity : AppCompatActivity() {
+class AddBusinessCardActivity : AppCompatActivity() {
 
     private val binding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
+        ActivityAddBusinessCardBinding.inflate(layoutInflater)
     }
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
